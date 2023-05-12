3ede1b56b51a99ea2121d903f96f89ab0c3b0c00
alvaro ccoyllo <55919786+pedroalvaroccoyllocondori@users.noreply.github.com>
2023-05-12 23:20:17 +0000

    json decode repaso

--- index.php
+++ index.php
@@ -1,22 +1,32 @@
 <?php
 
-
 ///json encode y decode
 //json encode array en un JSON
 
-
-
-///array
-$carros = [
+$array_asociativo = [
 "nombre"=>"sususki",
 "color"=>"blanco",
 "edad"=>2021
 ];
 
-$json = json_encode($carros);
 
-file_put_contents('data.json',$json);
+$array_asociativo=["pedro","lizetted"];
 
+///JSON ENCODE
+
+$archivo_json = json_encode($array_asociativo,JSON_FORCE_OBJECT | JSON_PRETTY_PRINT);
+
+file_put_contents('data_array simple.json',$archivo_json);
+
+//JSON DECODE
+
+$json_contenido= file_get_contents('data.json');
+
+$json_decode=json_decode($json_contenido,JSON_OBJECT_AS_ARRAY);
+
+// print_r($json_decode);
+
+echo $json_decode['nombre'];
 
 // echo gettype($bar);
 
