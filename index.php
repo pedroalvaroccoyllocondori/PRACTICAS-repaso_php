6bd4fe92efdb71a74b10cfe7f8b8d75fb68e3016
alvaro ccoyllo <55919786+pedroalvaroccoyllocondori@users.noreply.github.com>
2023-05-26 20:46:11 +0000

    recorreindo un arrray asociativo con foreach

--- index.php
+++ index.php
@@ -1,59 +1,16 @@
+
 <?php
 
-$directorio= "animales";
-$archivos =scandir($directorio);
+//recorrer un array asociativo con for each
 
-impresion_array($archivos);
+$arreglo=array("jose"=>["pelicano","foca"],"maria"=>"perico","pedro"=>"canario");
 
-
-function impresion_array($array){
-    echo '<pre>';
-    print_r($array);
-    echo '</pre>';
+foreach($arreglo as $dueño=>$animal){
+    
+    echo "<pre>";
+    echo "</pre>";
+    echo "<b> el dueno de </b>".$animal[0]."<b> es:</b>".$dueño;
+    
 }
-
-$archivos =array_diff($archivos,array('..','.'));
-
-// impresion_array($archivos);
-
-$archivos=array_values($archivos);
-
-// impresion_array($archivos);
-
-$animales=array();
-
-for ($i=0; $i <count($archivos) ; $i++) { 
-   preg_match("!(.*?)\((.*?)\)!",$archivos[$i],$resultado);
-
-
-   $nombre_animal=$resultado[1];
-   $nombre_animal=ucwords($nombre_animal);
-
-    $animales[$nombre_animal]['nombre_imagen']=$archivos[$i];
-    $animales[$nombre_animal]['tipo']=$resultado[2];
-
-
-}
-
-impresion_array($animales);
-
-echo "<table>";
-echo "<tr>";
-
-foreach ($animales as $key => $value) {
-    $contenido="<td><span>$key</span></br><img width='100' height='100' src='animales/$value[nombre_imagen]'></img></br><span>$value[tipo]</span></td>";
-
-    echo $contenido;
-}
-
-
-echo "</tr>";
-echo "</table>";
-
-
-
-
-
 
 ?>
-
